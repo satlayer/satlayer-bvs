7152f6c231c65393058a7c094e785a834cdb3eca
adam429 <adam@satlayer.io>
2024-07-23 22:25:47 +0800

    feat: modifi recover func & store wasm to cosmwasm testnet5

--- contracts/avs-directory/src/contract.rs
+++ contracts/avs-directory/src/contract.rs
@@ -2,16 +2,17 @@ use crate::{
     error::ContractError,
     msg::{ExecuteMsg, InstantiateMsg, QueryMsg, OperatorStatusResponse, SignatureWithSaltAndExpiry},
     state::{OperatorAVSRegistrationStatus, AVSDirectoryStorage},
-    utils::{calculate_digest_hash, recover, is_operator_registered},
+    utils::{calculate_digest_hash, recover},
 };  
 use cosmwasm_std::{
-    to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint64, Addr, 
+    entry_point, to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint64, Addr, 
 };
 use cw2::set_contract_version;
 
 const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
@@ -32,6 +33,7 @@ pub fn instantiate(
         .add_attribute("owner", owner))
 }
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
     env: Env,
@@ -72,15 +74,7 @@ pub fn register_operator(
         return Err(ContractError::SaltAlreadySpent {});
     }
 
-    // let delegation_manager_addr = storage.delegation_manager.load(deps.storage)?; 
-    
-    // if !is_operator_registered(&deps.querier, &env, &delegation_manager_addr, &operator)? {
-    //     return Err(ContractError::OperatorNotRegistered {});
-    // }
-
     let chain_id: u64 = env.block.chain_id.parse().unwrap_or(0);
-    println!("Current chain_id: {}", chain_id); 
-    println!("Current contract address: {}", env.contract.address);
 
     let message_bytes = calculate_digest_hash(
         &operator,
@@ -90,7 +84,7 @@ pub fn register_operator(
         chain_id,
         &env,
     );
-    
+
     if !recover(&message_bytes, &operator_signature.signature, &operator)? {
         return Err(ContractError::InvalidSignature {});
     }
@@ -178,6 +172,7 @@ pub fn transfer_ownership(
         .add_attribute("new_owner", new_owner.to_string()))
 }
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::QueryOperator { operator } => to_json_binary(&query_operator(deps, operator)?),
@@ -196,7 +191,6 @@ mod tests {
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
     use cosmwasm_std::{Addr, Storage, Binary, Uint64};
     use secp256k1::{Secp256k1, SecretKey, PublicKey, Message};
-    use bech32::{ToBase32, Variant, encode};
 
     #[test]
     fn test_instantiate() {
@@ -228,18 +222,16 @@ mod tests {
         assert_eq!(delegation_manager, Addr::unchecked("delegation_manager"));
     }
 
-    fn generate_operator() -> (Addr, SecretKey, PublicKey) {
+    fn generate_operator() -> (Addr, SecretKey, Vec<u8>) {
         let secp = Secp256k1::new();
         let secret_key = SecretKey::from_slice(&[0xcd; 32]).unwrap();
         let public_key = PublicKey::from_secret_key(&secp, &secret_key);
-        let operator_bytes = public_key.serialize();  
-
-        let bech32_address = encode("osmo", operator_bytes.to_base32(), Variant::Bech32).unwrap();
-
-        let operator = Addr::unchecked(bech32_address);
-        (operator, secret_key, public_key)
-    } 
-
+        let operator_bytes = public_key.serialize(); 
+    
+        let operator = Addr::unchecked(hex::encode(&operator_bytes));
+        (operator, secret_key, operator_bytes.to_vec())
+    }
+    
     fn mock_signature_with_message(
         operator: &Addr,
         sender: &Addr,
@@ -248,72 +240,74 @@ mod tests {
         chain_id: u64,
         _contract_addr: &Addr,
         secret_key: &SecretKey,
-    ) -> SignatureWithSaltAndExpiry {        
+    ) -> SignatureWithSaltAndExpiry {
         let env = mock_env();
         let message_bytes = calculate_digest_hash(
-            operator,
+            &operator,
             sender,
             &Binary::from(salt.as_bytes()),
             expiry,
             chain_id,
             &env,
         );
-
+    
         println!("Message Hash: {:?}", message_bytes);
+        println!("Operator: {:?}", operator);
 
+    
         let secp = Secp256k1::new();
-        let message = Message::from_digest_slice(&message_bytes).expect("32 bytes");
+        let message = Message::from_slice(&message_bytes).expect("32 bytes");
         let signature = secp.sign_ecdsa(&message, secret_key);
         let signature_bytes = signature.serialize_compact().to_vec();
-
+    
         println!("Signature: {:?}", signature_bytes);
-
+    
         SignatureWithSaltAndExpiry {
             salt: Binary::from(salt.as_bytes()),
             expiry: Uint64::from(expiry),
             signature: Binary::from(signature_bytes),
         }
     }
-
+    
     #[test]
     fn test_register_operator() {
         let mut deps = mock_dependencies();
         let env = mock_env();
         let info = mock_info("creator", &[]);
-
-        let (operator, secret_key, _public_key) = generate_operator();
+    
+        let (operator, secret_key, public_key_bytes) = generate_operator();
         println!("Operator Address: {:?}", operator);
         println!("Secret Key: {:?}", secret_key);
-        println!("Public Key: {:?}", _public_key);
-
+        println!("Public Key Bytes: {:?}", public_key_bytes);
+    
         let current_time = env.block.time.seconds();
         let expiry = current_time + 1000;
         let salt = "salt";
         let chain_id: u64 = 0;
         let contract_addr = env.contract.address.clone();
         let signature = mock_signature_with_message(&operator, &info.sender, salt, expiry, chain_id, &contract_addr, &secret_key);
-
+    
         println!("Operator: {:?}", operator);
         println!("Signature: {:?}", signature);
-
+    
         let instantiate_msg = InstantiateMsg {
             initial_owner: Addr::unchecked("owner"),
             delegation_manager: Addr::unchecked("delegation_manager"),
         };
         instantiate(deps.as_mut(), env.clone(), info.clone(), instantiate_msg).unwrap();
-
+    
         let msg = ExecuteMsg::RegisterOperatorToAVS {
             operator: operator.clone(),
             signature: signature.clone(),
         };
         let res = execute(deps.as_mut(), env.clone(), info.clone(), msg);
-
+    
         if let Err(ref err) = res {
             println!("Error: {:?}", err);
         }
-
+    
         assert!(res.is_ok());
-
+    
         let res = res.unwrap();
         assert_eq!(res.attributes.len(), 3);
         assert_eq!(res.attributes[0].key, "method");
@@ -322,11 +316,11 @@ mod tests {
         assert_eq!(res.attributes[1].value, operator.to_string());
         assert_eq!(res.attributes[2].key, "avs");
         assert_eq!(res.attributes[2].value, info.sender.to_string());
-
+    
         let storage = AVSDirectoryStorage::default();
         let status = storage.load_status(&deps.storage, info.sender.clone(), operator.clone()).unwrap();
         assert_eq!(status, OperatorAVSRegistrationStatus::Registered);
-
+    
         let is_salt_spent = storage.is_salt_spent(&deps.storage, operator.clone(), Binary::from(salt.as_bytes())).unwrap();
         assert!(is_salt_spent);
     }
@@ -337,10 +331,10 @@ mod tests {
         let env = mock_env();
         let info = mock_info("creator", &[]);
 
-        let (operator, secret_key, _public_key) = generate_operator();
+        let (operator, secret_key, public_key_bytes) = generate_operator();
         println!("Operator Address: {:?}", operator);
         println!("Secret Key: {:?}", secret_key);
-        println!("Public Key: {:?}", _public_key);
+        println!("Public Key Bytes: {:?}", public_key_bytes);
 
         let current_time = env.block.time.seconds();
         let expiry = current_time + 1000;
--- contracts/avs-directory/src/utils.rs
+++ contracts/avs-directory/src/utils.rs
@@ -1,20 +1,17 @@
-use cosmwasm_std::{Addr, Binary, StdResult, QuerierWrapper, WasmQuery, QueryRequest, Env, StdError, to_json_binary};
-use crate::msg::{IsOperatorRegisteredQueryMsg, IsOperatorRegisteredResponse};
-use tiny_keccak::{Hasher, Keccak};
-use secp256k1::{Message, Secp256k1, PublicKey};
-use secp256k1::ecdsa::Signature;
-use bech32::FromBase32;
+use cosmwasm_std::{Addr, Binary, StdResult, Env, StdError};
+// use crate::msg::{IsOperatorRegisteredQueryMsg, IsOperatorRegisteredResponse};
+// use cosmwasm_std::{QuerierWrapper, WasmQuery, QueryRequest, to_json_binary}
+use sha2::{Sha256, Digest};
+use cosmwasm_crypto::secp256k1_verify;
 
 const OPERATOR_AVS_REGISTRATION_TYPEHASH: &[u8] = b"OperatorAVSRegistration(address operator,address avs,bytes32 salt,uint256 expiry)";
 const DOMAIN_TYPEHASH: &[u8] = b"EIP712Domain(string name,uint256 chainId,address verifyingContract)";
 const DOMAIN_NAME: &[u8] = b"EigenLayer";
 
-pub fn keccak256(input: &[u8]) -> Vec<u8> {
-    let mut hasher = Keccak::v256();
-    let mut output = [0u8; 32];
+fn sha256(input: &[u8]) -> Vec<u8> {
+    let mut hasher = Sha256::new();
     hasher.update(input);
-    hasher.finalize(&mut output);
-    output.to_vec()
+    hasher.finalize().to_vec()
 }
 
 pub fn calculate_digest_hash(
@@ -25,29 +22,23 @@ pub fn calculate_digest_hash(
     chain_id: u64,
     env: &Env,
 ) -> Vec<u8> {
-    // Get the current contract address
-    let this_contract_address = &env.contract.address;
-
-    // Calculate struct hash
     let struct_hash_input = [
-        &keccak256(OPERATOR_AVS_REGISTRATION_TYPEHASH)[..],
+        &sha256(OPERATOR_AVS_REGISTRATION_TYPEHASH)[..],
         operator.as_bytes(),
         avs.as_bytes(),
         salt.as_slice(),
         &expiry.to_le_bytes(),
     ]
     .concat();
-    let struct_hash = keccak256(&struct_hash_input);
+    let struct_hash = sha256(&struct_hash_input);
 
-    // Calculate domain separator
-    let domain_separator = keccak256(&[
-        &keccak256(DOMAIN_TYPEHASH)[..],
-        &keccak256(DOMAIN_NAME)[..],
+    let domain_separator = sha256(&[
+        &sha256(DOMAIN_TYPEHASH)[..],
+        &sha256(DOMAIN_NAME)[..],
         &chain_id.to_le_bytes(),
-        this_contract_address.as_bytes(),
+        env.contract.address.as_bytes(),
     ].concat());
 
-    // Calculate digest hash
     let digest_hash_input = [
         b"\x19\x01",
         &domain_separator[..],
@@ -55,39 +46,34 @@ pub fn calculate_digest_hash(
     ]
     .concat();
 
-    keccak256(&digest_hash_input)
+    sha256(&digest_hash_input)
 }
 
 pub fn recover(digest_hash: &[u8], signature: &[u8], operator: &Addr) -> StdResult<bool> {
-    let secp = Secp256k1::verification_only();
-    let message = Message::from_digest_slice(digest_hash).map_err(|_| StdError::generic_err("Invalid message"))?;
-    let signature = Signature::from_compact(signature).map_err(|_| StdError::generic_err("Invalid signature"))?;
+    let public_key_bytes = hex::decode(operator.as_str()).map_err(|_| StdError::generic_err("Invalid operator address"))?;
 
-    let (_, operator_bytes, _) = bech32::decode(operator.as_str()).map_err(|_| StdError::generic_err("Invalid operator address"))?;
-    let operator_bytes = Vec::<u8>::from_base32(&operator_bytes).map_err(|_| StdError::generic_err("Invalid operator address"))?;
-    let public_key = PublicKey::from_slice(&operator_bytes).map_err(|_| StdError::generic_err("Invalid public key"))?;
-
-    match secp.verify_ecdsa(&message, &signature, &public_key) {
-        Ok(_) => Ok(true),
+    match secp256k1_verify(digest_hash, signature, &public_key_bytes) {
+        Ok(valid) => Ok(valid),
         Err(_) => Ok(false),
     }
 }
 
-pub fn is_operator_registered<Q: cosmwasm_std::CustomQuery>(
-    querier: &QuerierWrapper<Q>,
-    _env: &Env,
-    delegation_manager_addr: &Addr,
-    operator: &Addr,
-) -> StdResult<bool> {
-    let query_msg = IsOperatorRegisteredQueryMsg {
-        operator: operator.clone(),
-    };
 
-    let query_request = QueryRequest::Wasm(WasmQuery::Smart {
-        contract_addr: delegation_manager_addr.to_string(),
-        msg: to_json_binary(&query_msg)?,
-    });
+// pub fn is_operator_registered<Q: cosmwasm_std::CustomQuery>(
+//     querier: &QuerierWrapper<Q>,
+//     _env: &Env,
+//     delegation_manager_addr: &Addr,
+//     operator: &Addr,
+// ) -> StdResult<bool> {
+//     let query_msg = IsOperatorRegisteredQueryMsg {
+//         operator: operator.clone(),
+//     };
 
-    let res: IsOperatorRegisteredResponse = querier.query(&query_request)?;
-    Ok(res.registered)
-}
+//     let query_request = QueryRequest::Wasm(WasmQuery::Smart {
+//         contract_addr: delegation_manager_addr.to_string(),
+//         msg: to_json_binary(&query_msg)?,
+//     });
+
+//     let res: IsOperatorRegisteredResponse = querier.query(&query_request)?;
+//     Ok(res.registered)
+// }
