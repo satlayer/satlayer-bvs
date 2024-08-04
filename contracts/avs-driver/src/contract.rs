eee82f0022cd32ff792a311ef3aa9fd047aaf30b
0xSmithJohn <smithj@satlayer.io>
2024-08-04 12:09:16 +0800

    feat: build avsdriver core execute function

--- contracts/avs-driver/src/contract.rs
+++ contracts/avs-driver/src/contract.rs
@@ -1,1 +1,35 @@
+use crate::{error::ContractError, msg::ExecuteMsg, msg::InstantiateMsg, msg::QueryMsg};
 
+use cosmwasm_std::{
+    entry_point, Binary, Deps, DepsMut, Env, Event, MessageInfo, Response, StdResult,
+};
+use cw2::set_contract_version;
+
+const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let response = Response::new().add_attribute("method", "instantiate");
+
+    Ok(response)
+}
+
+#[entry_point]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> StdResult<Response> {
+    match msg {
+        ExecuteMsg::ExecuteAvsOffchain { task_id } => execute_set(info, task_id),
+    }
+}
