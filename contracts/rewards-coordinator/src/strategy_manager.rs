43bd0296fb9fe83a5ea7f7211ba1c5ca4d00ae88
0xSmithJohn <smithj@satlayer.io>
2024-08-06 20:55:11 +0800

    feat: create struct InstantiateMsg

--- contracts/rewards-coordinator/src/strategy_manager.rs
+++ contracts/rewards-coordinator/src/strategy_manager.rs
@@ -1,1 +1,11 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, Uint128, Uint64, Binary};
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub delegation_manager: Addr,
+    pub slasher: Addr,
+    pub initial_strategy_whitelister: Addr,
+    pub initial_owner: Addr,
+}
 
