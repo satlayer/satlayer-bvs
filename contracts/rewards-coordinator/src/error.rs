6dbe7324dabd2507595f1edc6d96d32e3ae45972
0xSmithJohn <smithj@satlayer.io>
2024-08-07 18:48:11 +0800

    refactor: define the reward-coordinator errors

--- contracts/rewards-coordinator/src/error.rs
+++ contracts/rewards-coordinator/src/error.rs
@@ -50,4 +50,70 @@ pub enum ContractError {
 
     #[error("RewardsCoordinator.validate_rewards_submission: strategies must be in ascending order to handle duplicates")]
     StrategiesMuseBeHandleDuplicates {},
+
+    #[error("RewardsCoordinator.disable_root: invalid root index")]
+    InvalidRootIndex {},
+
+    #[error("RewardsCoordinator.disable_root: root does not exist")]
+    RootNotExist {},
+
+    #[error("RewardsCoordinator.disable_root: This root is already disabled")]
+    AlreadyDisabled {},
+
+    #[error("RewardsCoordinator.disable_root: This root is already activated and cannot be disabled")]
+    AlreadyActivated {},
+
+    #[error("RewardsCoordinator.submit_root: new root must be for newer calculated period")]
+    InvalidTimestamp {},
+
+    #[error("RewardsCoordinator.submit_root: timestamp in future")]
+    TimestampInFuture {},
+
+    #[error("RewardsCoordinator._verify_token_claim_proof: invalid tokenLeafIndex")]
+    InvalidTokenLeafIndex {},
+
+    #[error("RewardsCoordinator._verify_token_claim_proof: invalid token claim proof")]
+    InvalidTokenClaimProof {},
+
+    #[error("RewardsCoordinator._verify_earner_claim_proof: invalid earnerLeafIndex")]
+    InvalidEarnerLeafIndex {},
+
+    #[error("RewardsCoordinator._verify_earner_claim_proof: invalid earner claim proof")]
+    InvalidEarnerClaimProof {},
+
+    #[error("RewardsCoordinator._check_claim: root disabled")]
+    RootDisabled {},
+
+    #[error("RewardsCoordinator._check_claim: root not activated yet")]
+    RootNotActivatedYet {},
+
+    #[error("RewardsCoordinator._check_claim: tokenIndices and tokenProofs length mismatch")]
+    TokenIndicesAndProofsMismatch {},
+
+    #[error("RewardsCoordinator._check_claim: tokenTreeProofs and leaves length mismatch")]
+    TokenProofsAndLeavesMismatch {},
+
+    #[error("RewardsCoordinator.process_claim: caller is not valid claimer")]
+    UnauthorizedClaimer {},
+
+    #[error("RewardsCoordinator.process_claim: cumulativeEarnings must be gt than cumulativeClaimed")]
+    CumulativeEarningsTooLow {},
+
+    #[error("RewardsCoordinator.query_calculate_token_leaf_hash: invalid cumulative earnings")]
+    InvalidCumulativeEarnings {},
+
+    #[error("RewardsCoordinator.query_get_distribution_root_at_index: invalid index format")]
+    InvalidIndexFormat {},
+
+    #[error("RewardsCoordinator.query_get_current_distribution_root: no active root found")]
+    NoActiveRootFound {},
+
+    #[error("RewardsCoordinator.query_get_current_claimable_distribution_root: no claimable root found")]
+    NoClaimableRootFound {},
+
+    #[error("RewardsCoordinator.query_get_root_index_from_hash: root not found")]
+    RootNotFound {},
+
+    #[error("RewardsCoordinator.query_get_root_index_from_hash: invalid hex format")]
+    InvalidHexFormat {},
 }
