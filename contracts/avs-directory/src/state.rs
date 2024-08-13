8361c85309d68295ba814b8f2d5e615939f82f14
adam429 <adam@satlayer.io>
2024-08-13 08:04:46 +0800

    refactor: add AVSInfo structure

--- contracts/avs-directory/src/state.rs
+++ contracts/avs-directory/src/state.rs
@@ -8,8 +8,16 @@ pub enum OperatorAVSRegistrationStatus {
     Unregistered,
 }
 
-pub const OWNER: Item<Addr> = Item::new("owner");
+#[cw_serde]
+pub struct AVSInfo {
+    pub avs_hash: String,
+    pub contract_addr: String,
+    pub state_bank: String,
+    pub avs_driver: String,
+}
 
+pub const OWNER: Item<Addr> = Item::new("owner");
 pub const AVS_OPERATOR_STATUS: Map<(Addr, Addr), OperatorAVSRegistrationStatus> = Map::new("avs_operator_status");
 pub const OPERATOR_SALT_SPENT: Map<(Addr, String), bool> = Map::new("operator_salt_is_spent");
 pub const DELEGATION_MANAGER: Item<Addr> = Item::new("delegation_manager");
+pub const AVS_INFO: Map<String, AVSInfo> = Map::new("avs_info");
