42686925d9ee1a7fc9aff3d2367edf7c943d6b01
0xCarl <carl@satlayer.io>
2024-09-14 10:44:35 +0800

    feat: token blacklist and strategy management

--- contracts/strategy-factory/src/contract.rs
+++ contracts/strategy-factory/src/contract.rs
@@ -1,5 +1,6 @@
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use crate::query::{BlacklistStatusResponse, StrategyResponse};
 use crate::state::{Config, CONFIG, DEPLOYEDSTRATEGIES, IS_BLACKLISTED};
 use cosmwasm_std::{
     entry_point, to_json_binary, Addr, Api, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo,
@@ -107,6 +108,13 @@ pub fn execute(
                 third_party_transfers_forbidden_values,
             )
         }
+        ExecuteMsg::SetStrategyManager {
+            new_strategy_manager,
+        } => {
+            let new_strategy_manager_addr = deps.api.addr_validate(&new_strategy_manager)?;
+
+            set_strategy_manager(deps, info, new_strategy_manager_addr)
+        }
         ExecuteMsg::TransferOwnership { new_owner } => {
             let new_owner_addr = deps.api.addr_validate(&new_owner)?;
             transfer_ownership(deps, info, new_owner_addr)
@@ -141,6 +149,8 @@ pub fn deploy_new_strategy(
     unpauser: Addr,
 ) -> Result<Response, ContractError> {
     only_when_not_paused(deps.as_ref(), PAUSED_NEW_STRATEGIES)?;
+    
+    only_owner(deps.as_ref(), &info)?;
 
     let config = CONFIG.load(deps.storage)?;
 
@@ -188,6 +198,8 @@ pub fn blacklist_tokens(
     info: MessageInfo,
     tokens: Vec<Addr>,
 ) -> Result<Response, ContractError> {
+    let config = CONFIG.load(deps.storage)?;
+
     only_owner(deps.as_ref(), &info)?;
 
     let mut strategies_to_remove: Vec<Addr> = Vec::new();
@@ -209,13 +221,25 @@ pub fn blacklist_tokens(
         }
     }
 
+    let mut response = Response::new()
+        .add_attribute("method", "blacklist_tokens")
+        .add_attribute("tokens_blacklisted", format!("{:?}", tokens));
+
     if !strategies_to_remove.is_empty() {
-        remove_strategies_from_whitelist(deps, info, strategies_to_remove)?;
+        let msg = StrategyManagerExecuteMsg::RemoveStrategiesFromWhitelist {
+            strategies: strategies_to_remove.iter().map(|s| s.to_string()).collect(),
+        };
+
+        let exec_msg = WasmMsg::Execute {
+            contract_addr: config.strategy_manager.to_string(),
+            msg: to_json_binary(&msg)?,
+            funds: vec![],
+        };
+
+        response = response.add_message(CosmosMsg::Wasm(exec_msg));
     }
 
-    Ok(Response::new()
-        .add_attribute("method", "blacklist_tokens")
-        .add_attribute("tokens_blacklisted", format!("{:?}", tokens)))
+    Ok(response)
 }
 
 pub fn remove_strategies_from_whitelist(
@@ -269,6 +293,24 @@ pub fn set_third_party_transfers_forbidden(
         .add_attribute("method", "set_third_party_transfers_forbidden")
         .add_attribute("strategy", strategy.to_string())
         .add_attribute("value", value.to_string()))
+}
+
+pub fn set_strategy_manager(
+    deps: DepsMut,
+    info: MessageInfo,
+    new_strategy_manager: Addr,
+) -> Result<Response, ContractError> {
+    only_owner(deps.as_ref(), &info)?;
+
+    let mut config = CONFIG.load(deps.storage)?;
+
+    config.strategy_manager = new_strategy_manager.clone();
+
+    CONFIG.save(deps.storage, &config)?;
+
+    Ok(Response::new()
+        .add_attribute("method", "set_strategy_manager")
+        .add_attribute("new_strategy_manager", new_strategy_manager.to_string()))
 }
 
 pub fn whitelist_strategies(
@@ -400,13 +442,6 @@ fn update_config(
     Ok(Response::new().add_attribute("method", "update_config"))
 }
 
-fn validate_addresses(api: &dyn Api, addresses: &[String]) -> StdResult<Vec<Addr>> {
-    addresses
-        .iter()
-        .map(|addr| api.addr_validate(addr))
-        .collect()
-}
-
 pub fn transfer_ownership(
     deps: DepsMut,
     info: MessageInfo,
@@ -438,16 +473,23 @@ pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     }
 }
 
-fn query_strategy(deps: Deps, token_address: Addr) -> StdResult<Addr> {
-    let strategy = DEPLOYEDSTRATEGIES.load(deps.storage, &token_address)?;
-    Ok(strategy)
+fn query_strategy(deps: Deps, token: Addr) -> StdResult<StrategyResponse> {
+    let strategy = DEPLOYEDSTRATEGIES.load(deps.storage, &token)?;
+    Ok(StrategyResponse { strategy })
 }
 
-fn query_blacklist_status(deps: Deps, token: Addr) -> StdResult<bool> {
+fn query_blacklist_status(deps: Deps, token: Addr) -> StdResult<BlacklistStatusResponse> {
     let is_blacklisted = IS_BLACKLISTED
         .may_load(deps.storage, &token)?
         .unwrap_or(false);
-    Ok(is_blacklisted)
+    Ok(BlacklistStatusResponse { is_blacklisted })
+}
+
+fn validate_addresses(api: &dyn Api, addresses: &[String]) -> StdResult<Vec<Addr>> {
+    addresses
+        .iter()
+        .map(|addr| api.addr_validate(addr))
+        .collect()
 }
 
 fn set_strategy_for_token(
@@ -491,14 +533,20 @@ mod tests {
         message_info, mock_dependencies, mock_env, MockApi, MockQuerier, MockStorage,
     };
     use cosmwasm_std::{
-        from_json, Addr, Binary, DepsMut, Env, MessageInfo, OwnedDeps, Response, Uint128,
+        from_json, Addr, Binary, ContractResult, MessageInfo, OwnedDeps, Response, SystemError,
+        SystemResult, WasmQuery,
     };
 
-    fn setup_contract() -> OwnedDeps<MockStorage, MockApi, MockQuerier> {
+    fn setup_contract() -> (
+        OwnedDeps<MockStorage, MockApi, MockQuerier>,
+        Addr,
+        MessageInfo,
+    ) {
         let mut deps = mock_dependencies();
-        let info = message_info(&Addr::unchecked("creator"), &[]);
 
         let initial_owner = deps.api.addr_make("initial_owner");
+
+        let info = message_info(&initial_owner, &[]);
         let strategy_manager = deps.api.addr_make("strategy_manager");
         let pauser = deps.api.addr_make("pauser");
         let unpauser = deps.api.addr_make("unpauser");
@@ -513,16 +561,14 @@ mod tests {
             initial_paused_status: 0,
         };
 
-        instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        instantiate(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
 
-        deps
+        (deps, initial_owner, info)
     }
 
     #[test]
     fn test_deploy_new_strategy() {
-        let mut deps = setup_contract();
-        let info = message_info(&Addr::unchecked("creator"), &[]);
-
+        let (mut deps, _initial_owner, info) = setup_contract();
         let token = deps.api.addr_make("token");
         let pauser = deps.api.addr_make("pauser");
         let unpauser = deps.api.addr_make("unpauser");
@@ -550,5 +596,296 @@ mod tests {
             .load(&deps.storage, &Addr::unchecked(token))
             .unwrap();
         assert_eq!(strategy_addr, &Addr::unchecked(""));
+    }
+
+    #[test]
+    fn test_blacklist_tokens() {
+        let (mut deps, initial_owner, _) = setup_contract();
+
+        let info = message_info(&initial_owner, &[]);
+
+        let token1 = deps.api.addr_make("token1_address");
+        let token2 = deps.api.addr_make("token2_address");
+
+        let strategy1 = deps.api.addr_make("strategy1");
+        let strategy2 = deps.api.addr_make("strategy2");
+
+        DEPLOYEDSTRATEGIES
+            .save(deps.as_mut().storage, &token1, &strategy1)
+            .unwrap();
+        DEPLOYEDSTRATEGIES
+            .save(deps.as_mut().storage, &token2, &strategy2)
+            .unwrap();
+
+        deps.querier.update_wasm(|query| match query {
+            WasmQuery::Smart {
+                contract_addr,
+                msg: _,
+                ..
+            } => {
+                if contract_addr == "strategy_manager" {
+                    SystemResult::Ok(ContractResult::Ok(
+                        to_json_binary(&"RemoveStrategiesFromWhitelist executed successfully")
+                            .unwrap(),
+                    ))
+                } else {
+                    SystemResult::Err(SystemError::InvalidResponse {
+                        error: "unknown address".to_string(),
+                        response: Binary::default(),
+                    })
+                }
+            }
+            _ => SystemResult::Err(SystemError::InvalidResponse {
+                error: "unsupported query".to_string(),
+                response: Binary::default(),
+            }),
+        });
+
+        let msg = ExecuteMsg::BlacklistTokens {
+            tokens: vec![token1.to_string(), token2.to_string()],
+        };
+
+        let result = execute(deps.as_mut(), mock_env(), info, msg);
+
+        assert!(result.is_ok());
+        let response: Response = result.unwrap();
+
+        assert_eq!(response.messages.len(), 1);
+        let msg = &response.messages[0];
+
+        if let CosmosMsg::Wasm(WasmMsg::Execute {
+            contract_addr, msg, ..
+        }) = &msg.msg
+        {
+            assert_eq!(
+                contract_addr.to_string(),
+                deps.api.addr_make("strategy_manager").to_string()
+            );
+
+            let remove_msg: StrategyManagerExecuteMsg = from_json(msg).unwrap();
+            if let StrategyManagerExecuteMsg::RemoveStrategiesFromWhitelist { strategies } =
+                remove_msg
+            {
+                assert_eq!(
+                    strategies,
+                    vec![strategy1.to_string(), strategy2.to_string()]
+                );
+            } else {
+                panic!("Expected RemoveStrategiesFromWhitelist message");
+            }
+        } else {
+            panic!("Expected WasmMsg::Execute");
+        }
+
+        let is_blacklisted1 = IS_BLACKLISTED.load(deps.as_mut().storage, &token1).unwrap();
+        let is_blacklisted2 = IS_BLACKLISTED.load(deps.as_mut().storage, &token2).unwrap();
+        assert!(is_blacklisted1);
+        assert!(is_blacklisted2);
+    }
+
+    #[test]
+    fn test_set_strategy_manager() {
+        let (mut deps, initial_owner, _) = setup_contract();
+
+        let info = message_info(&initial_owner, &[]);
+
+        let new_strategy_manager = deps.api.addr_make("new_strategy_manager");
+
+        let msg = ExecuteMsg::SetStrategyManager {
+            new_strategy_manager: new_strategy_manager.to_string(),
+        };
+
+        let result = execute(deps.as_mut(), mock_env(), info.clone(), msg);
+
+        assert!(result.is_ok());
+        let response: Response = result.unwrap();
+
+        assert_eq!(response.attributes.len(), 2);
+        assert_eq!(response.attributes[0].key, "method");
+        assert_eq!(response.attributes[0].value, "set_strategy_manager");
+        assert_eq!(response.attributes[1].key, "new_strategy_manager");
+    }
+
+    #[test]
+    fn test_update_config() {
+        let (mut deps, initial_owner, _) = setup_contract();
+
+        let info = message_info(&initial_owner, &[]);
+
+        let new_owner = deps.api.addr_make("new_owner");
+        let new_strategy_code_id = 2;
+        let new_only_owner_can_create = false;
+
+        let msg = ExecuteMsg::UpdateConfig {
+            new_owner: new_owner.to_string(),
+            strategy_code_id: new_strategy_code_id,
+            only_owner_can_create: new_only_owner_can_create,
+        };
+
+        let result = execute(deps.as_mut(), mock_env(), info.clone(), msg);
+
+        assert!(result.is_ok());
+        let response: Response = result.unwrap();
+
+        assert_eq!(response.attributes.len(), 1);
+        assert_eq!(response.attributes[0].key, "method");
+        assert_eq!(response.attributes[0].value, "update_config");
+
+        let config = CONFIG.load(&deps.storage).unwrap();
+        assert_eq!(config.owner, new_owner);
+        assert_eq!(config.strategy_code_id, new_strategy_code_id);
+        assert_eq!(config.only_owner_can_create, new_only_owner_can_create);
+
+        let unauthorized_user = message_info(&Addr::unchecked("unauthorized_user"), &[]);
+        let msg = ExecuteMsg::UpdateConfig {
+            new_owner: new_owner.to_string(),
+            strategy_code_id: new_strategy_code_id,
+            only_owner_can_create: new_only_owner_can_create,
+        };
+
+        let result = execute(deps.as_mut(), mock_env(), unauthorized_user, msg);
+        assert!(result.is_err());
+
+        if let Err(err) = result {
+            match err {
+                ContractError::Unauthorized {} => (),
+                _ => panic!("Unexpected error: {:?}", err),
+            }
+        }
+    }
+
+    #[test]
+    fn test_transfer_ownership() {
+        let (mut deps, initial_owner, _) = setup_contract();
+
+        let info = message_info(&initial_owner, &[]);
+
+        let new_owner = deps.api.addr_make("new_owner");
+
+        let msg = ExecuteMsg::TransferOwnership {
+            new_owner: new_owner.to_string(),
+        };
+
+        let result = execute(deps.as_mut(), mock_env(), info.clone(), msg);
+
+        assert!(result.is_ok());
+        let response: Response = result.unwrap();
+
+        assert_eq!(response.attributes.len(), 2);
+        assert_eq!(response.attributes[0].key, "method");
+        assert_eq!(response.attributes[0].value, "transfer_ownership");
+        assert_eq!(response.attributes[1].key, "new_owner");
+        assert_eq!(response.attributes[1].value, new_owner.to_string());
+
+        let config = CONFIG.load(&deps.storage).unwrap();
+        assert_eq!(config.owner, new_owner);
+
+        let unauthorized_user = message_info(&deps.api.addr_make("unauthorized_user"), &[]);
+        let msg = ExecuteMsg::TransferOwnership {
+            new_owner: deps.api.addr_make("another_owner").to_string(),
+        };
+
+        let result = execute(deps.as_mut(), mock_env(), unauthorized_user, msg);
+        assert!(result.is_err());
+
+        if let Err(err) = result {
+            match err {
+                ContractError::Unauthorized {} => (),
+                _ => panic!("Unexpected error: {:?}", err),
+            }
+        }
+    }
+
+    #[test]
+    fn test_query_blacklist_status() {
+        let (mut deps, _initial_owner, _) = setup_contract();
+
+        let token1 = deps.api.addr_make("token1_address");
+        let token2 = deps.api.addr_make("token2_address");
+
+        IS_BLACKLISTED
+            .save(deps.as_mut().storage, &token1, &true)
+            .unwrap();
+
+        IS_BLACKLISTED
+            .save(deps.as_mut().storage, &token2, &false)
+            .unwrap();
+
+        let msg = QueryMsg::IsTokenBlacklisted {
+            token: token1.to_string(),
+        };
+
+        let bin = query(deps.as_ref(), mock_env(), msg).unwrap();
+        let res: BlacklistStatusResponse = from_json(&bin).unwrap();
+
+        assert!(res.is_blacklisted);
+
+        let msg = QueryMsg::IsTokenBlacklisted {
+            token: token2.to_string(),
+        };
+
+        let bin = query(deps.as_ref(), mock_env(), msg).unwrap();
+        let res: BlacklistStatusResponse = from_json(&bin).unwrap();
+
+        assert!(!res.is_blacklisted);
+    }
+
+    #[test]
+    fn test_query_strategy_combined() {
+        let (mut deps, _initial_owner, _) = setup_contract();
+
+        let token = deps.api.addr_make("token_address");
+        let strategy = deps.api.addr_make("strategy_address");
+
+        DEPLOYEDSTRATEGIES
+            .save(deps.as_mut().storage, &token, &strategy)
+            .unwrap();
+
+        let msg = QueryMsg::GetStrategy {
+            token: token.to_string(),
+        };
+
+        let bin = query(deps.as_ref(), mock_env(), msg).unwrap();
+        let res: StrategyResponse = from_json(&bin).unwrap();
+
+        assert_eq!(res.strategy, strategy);
+
+        let token_not_found = deps.api.addr_make("non_existing_token");
+
+        let msg = QueryMsg::GetStrategy {
+            token: token_not_found.to_string(),
+        };
+
+        let result = query(deps.as_ref(), mock_env(), msg);
+
+        assert!(result.is_err());
+        if let Err(StdError::NotFound { .. }) = result {
+        } else {
+            panic!("Expected NotFound error");
+        }
+    }
+
+    #[test]
+    fn test_set_strategy_for_token() {
+        let (mut deps, _initial_owner, _) = setup_contract();
+
+        let token = deps.api.addr_make("token_address");
+        let strategy = deps.api.addr_make("strategy_address");
+
+        let result = set_strategy_for_token(deps.as_mut(), token.clone(), strategy.clone());
+
+        assert!(result.is_ok());
+
+        let response = result.unwrap();
+        assert_eq!(response.attributes.len(), 3);
+        assert_eq!(response.attributes[0].key, "method");
+        assert_eq!(response.attributes[0].value, "set_strategy_for_token");
+        assert_eq!(response.attributes[1].key, "token");
+        assert_eq!(response.attributes[1].value, token.to_string());
+        assert_eq!(response.attributes[2].key, "strategy");
+        assert_eq!(response.attributes[2].value, strategy.to_string());
+
+        let stored_strategy = DEPLOYEDSTRATEGIES.load(&deps.storage, &token).unwrap();
+        assert_eq!(stored_strategy, strategy);
     }
 }
