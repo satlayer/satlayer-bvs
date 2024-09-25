43f04bd1d1cf814de2c8a5d7273f3754064dce01
Junyiq <jun@satlayer.io>
2024-09-25 15:33:46 +0800

    feat: add deployed contract addresses on babylond testnet

--- contracts/avs-driver/src/contract.rs
+++ contracts/avs-driver/src/contract.rs
@@ -1,7 +1,10 @@
-use crate::{error::ContractError, msg::ExecuteMsg, msg::InstantiateMsg, msg::QueryMsg};
+use crate::{
+    error::ContractError, msg::ExecuteMsg, msg::InstantiateMsg, msg::QueryMsg,
+    state::IS_AVS_CONTRACT_REGISTERED,
+};
 
 use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Env, Event, MessageInfo, Response, StdResult,
+    entry_point, Addr, Binary, Deps, DepsMut, Env, Event, MessageInfo, Response, StdResult,
 };
 use cw2::set_contract_version;
 
@@ -24,24 +27,57 @@ pub fn instantiate(
 
 #[entry_point]
 pub fn execute(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
-) -> StdResult<Response> {
+) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::ExecuteAvsOffchain { task_id } => execute_set(info, task_id),
+        ExecuteMsg::ExecuteAvsOffchain { task_id } => execute_avs_offchain(deps, info, task_id),
+        ExecuteMsg::AddRegisteredAvsContract { address } => {
+            add_registered_avs_contract(deps, info, Addr::unchecked(address))
+        }
     }
 }
 
-pub fn execute_set(info: MessageInfo, task_id: u64) -> StdResult<Response> {
+pub fn execute_avs_offchain(
+    deps: DepsMut,
+    info: MessageInfo,
+    task_id: u64,
+) -> Result<Response, ContractError> {
+    let sender = info.sender;
+    let is_registered = IS_AVS_CONTRACT_REGISTERED
+        .may_load(deps.storage, &sender)?
+        .unwrap_or(false);
+
+    if !is_registered {
+        return Err(ContractError::AvsContractNotRegistered {});
+    }
+
     let event = Event::new("ExecuteAVSOffchain")
-        .add_attribute("sender", info.sender.to_string())
+        .add_attribute("sender", sender.to_string())
         .add_attribute("taskId", task_id.to_string());
 
     Ok(Response::new()
         .add_attribute("method", "ExecuteAvsOffchain")
         .add_attribute("taskId", task_id.to_string())
+        .add_event(event))
+}
+
+pub fn add_registered_avs_contract(
+    deps: DepsMut,
+    info: MessageInfo,
+    address: Addr,
+) -> Result<Response, ContractError> {
+    IS_AVS_CONTRACT_REGISTERED.save(deps.storage, &Addr::unchecked(address.clone()), &true)?;
+
+    let event = Event::new("RegisteredAvsContractAdded")
+        .add_attribute("sender", info.sender.to_string())
+        .add_attribute("address", address.to_string());
+
+    Ok(Response::new()
+        .add_attribute("method", "add_registered_avs_contract")
+        .add_attribute("address", address.to_string())
         .add_event(event))
 }
 
@@ -75,19 +111,34 @@ mod tests {
     fn test_executeavsoffchain() {
         let mut deps = mock_dependencies();
         let env = mock_env();
-        let info = message_info(&Addr::unchecked("alice"), &[]);
-        let msg = ExecuteMsg::ExecuteAvsOffchain { task_id: 1000 };
+        let avs_contract = Addr::unchecked("avs_contract");
+        let info = message_info(&avs_contract, &[]);
+        let task_id = 1000;
 
+        execute(
+            deps.as_mut(),
+            env.clone(),
+            info.clone(),
+            ExecuteMsg::AddRegisteredAvsContract {
+                address: avs_contract.to_string(),
+            },
+        )
+        .unwrap();
+
+        let msg = ExecuteMsg::ExecuteAvsOffchain { task_id };
         let res = execute(deps.as_mut(), env, info, msg).unwrap();
 
         assert_eq!(2, res.attributes.len());
         assert_eq!(("method", "ExecuteAvsOffchain"), res.attributes[0]);
-        assert_eq!(("taskId", "1000"), res.attributes[1]);
+        assert_eq!(("taskId", task_id.to_string()), res.attributes[1]);
 
         assert_eq!(1, res.events.len());
         assert_eq!("ExecuteAVSOffchain", res.events[0].ty);
         assert_eq!(
-            vec![("sender", "alice"), ("taskId", "1000"),],
+            vec![
+                ("sender", avs_contract.as_str()),
+                ("taskId", &task_id.to_string()),
+            ],
             res.events[0].attributes
         );
     }
@@ -119,9 +170,43 @@ mod tests {
                     ExecuteMsg::ExecuteAvsOffchain { task_id: id } => {
                         assert_eq!(id, task_id);
                     }
+                    _ => panic!("Unexpected ExecuteMsg type"),
                 }
             }
             _ => panic!("Unexpected CosmosMsg type"),
         }
+    }
+
+    #[test]
+    fn test_add_registered_avs_contract() {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = message_info(&Addr::unchecked("admin"), &[]);
+        let avs_contract_address = "avs_contract_123";
+        let msg = ExecuteMsg::AddRegisteredAvsContract {
+            address: avs_contract_address.to_string(),
+        };
+
+        let res = execute(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(2, res.attributes.len());
+        assert_eq!(("method", "add_registered_avs_contract"), res.attributes[0]);
+        assert_eq!(("address", avs_contract_address), res.attributes[1]);
+
+        assert_eq!(1, res.events.len());
+        assert_eq!("RegisteredAvsContractAdded", res.events[0].ty);
+        assert_eq!(
+            vec![("sender", "admin"), ("address", avs_contract_address),],
+            res.events[0].attributes
+        );
+
+        let is_registered = IS_AVS_CONTRACT_REGISTERED
+            .may_load(
+                deps.as_ref().storage,
+                &Addr::unchecked(avs_contract_address),
+            )
+            .unwrap()
+            .unwrap_or(false);
+        assert!(is_registered);
     }
 }
