0b0fa06ebc92be75d0c6d90ffeed9460e36a7f4e
fuxingloh <git@fuxing.dev>
2025-02-07 17:35:11 +0800

    feat: optimize `cosmwasm-optimizer` builds on CI and locally

---
+++ packages/docker-cosmwasm-optimizer/bin.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const path = require("path");
+
+const cwd = process.cwd();
+const projectName = path.basename(cwd);
+const parentDir = path.dirname(cwd);
+
+const baseCommand = ["docker run --rm", `-v ${parentDir}:/code`];
+
+// If running in CI, push the cache to the registry
+if (process.env.CI) {
+  baseCommand.push(`--cache-to=ghcr.io/satlayer/cosmwasm-optimizer-cache:${projectName}`);
+  // TODO(fuxingloh): We can pull from the cache (but our repo is not public, so that will complicate things)
+  baseCommand.push(`--cache-from=ghcr.io/satlayer/cosmwasm-optimizer-cache:${projectName}`);
+} else {
+  baseCommand.push(`--mount type=volume,source=sl_bvs_wasm_cache_${projectName},target=/target`);
+  baseCommand.push("--mount type=volume,source=sl_bvs_registry_cache,target=/usr/local/cargo/registry");
+}
+
+// Compose final command
+baseCommand.push(`ghcr.io/satlayer/cosmwasm-optimizer:local ./${projectName}`);
+
+const command = baseCommand.join(" ");
+
+execSync(command, { stdio: "inherit" });
