3922de2248377296a5a4bd6ce2e7695a64148c5f
fuxingloh <git@fuxing.dev>
2025-02-07 17:50:17 +0800

    remove cache-to from, wrong command

--- packages/docker-cosmwasm-optimizer/bin.js
+++ packages/docker-cosmwasm-optimizer/bin.js
@@ -9,12 +9,8 @@ const parentDir = path.dirname(cwd);
 
 const baseCommand = ["docker run --rm", `-v ${parentDir}:/code`];
 
-// If running in CI, push the cache to the registry
-if (process.env.CI) {
-  baseCommand.push(`--cache-to=ghcr.io/satlayer/cosmwasm-optimizer-cache:${projectName}`);
-  // TODO(fuxingloh): We can pull from the cache (but our repo is not public, so that will complicate things)
-  baseCommand.push(`--cache-from=ghcr.io/satlayer/cosmwasm-optimizer-cache:${projectName}`);
-} else {
+// Isn't much faster if we're using fresh instances on CI.
+if (!process.env.CI) {
   baseCommand.push(`--mount type=volume,source=sl_bvs_cache_wasm_${projectName},target=/target`);
   baseCommand.push(`--mount type=volume,source=sl_bvs_cache_registry_${projectName},target=/usr/local/cargo/registry`);
 }
