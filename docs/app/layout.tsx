4160c567d836da0207e3bc8cd6dcb24686f3f3ba
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2025-02-08 19:57:35 +0800

    feat: setup `./docs` for monorepo (#140)

---
+++ docs/app/layout.tsx
@@ -0,0 +1,60 @@
+import { Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+import { ReactNode } from "react";
+import { Metadata } from "next";
+import { SatLayerIcon } from "./icon";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://build.satlayer.xyz"),
+  title: {
+    default: "SatLayer Bitcoin Validated Service",
+    template: "%s - SatLayer",
+  },
+};
+const banner = (
+  <Banner storageKey="wip" dismissible={false}>
+    🚧 Work in progress! This site is under construction.
+  </Banner>
+);
+const navbar = (
+  <Navbar
+    logo={
+      <div className="flex items-center space-x-2">
+        <SatLayerIcon width="24px" height="24px" />
+        <b>SatLayer</b>
+      </div>
+    }
+  />
+);
+const footer = <></>;
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      dir="ltr"
+      // https://github.com/pacocoursey/next-themes/blob/1b510445a37e7c2cddb359b5f29fe8dce9fd4855/next-themes/README.md#with-app
+      suppressHydrationWarning
+    >
+      <Head></Head>
+      <body>
+        <Layout
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/satlayer/satlayer-bvs/tree/main/docs"
+          footer={footer}
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  );
+}
---
+++ docs/tailwind.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,md,mdx}"],
+  plugins: [],
+} satisfies Config;
