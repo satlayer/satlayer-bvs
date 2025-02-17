68777ab52412238d9197f60d8640041a112b4572
Fuxing <loh@fuxing.dev>
2025-02-17 23:09:00 +0800

    add package.json formatter

--- modules/bvs-api/logger/field.go
+++ modules/bvs-api/logger/field.go
@@ -8,11 +8,3 @@ type Field struct {
 func WithField(key string, val interface{}) Field {
 	return Field{Key: key, Val: val}
 }
-
-func WithF(key string, val interface{}) Field {
-	return Field{Key: key, Val: val}
-}
-
-func WithA(key string, val interface{}) Field {
-	return Field{Key: key, Val: val}
-}
