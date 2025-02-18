7082db5c6e463db1b2311d05cbffa0a20f5ae742
Fuxing <loh@fuxing.dev>
2025-02-19 01:08:42 +0800

    make chainio_test.go hermetic

--- modules/bvs-api/tests/e2e/chainio_test.go
+++ modules/bvs-api/tests/e2e/chainio_test.go
@@ -2,47 +2,37 @@ package e2e
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"testing"
-	"time"
 
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
-
+	"github.com/satlayer/satlayer-bvs/babylond"
+	"github.com/satlayer/satlayer-bvs/babylond/bvs"
 	"github.com/satlayer/satlayer-bvs/bvs-api/chainio/io"
 	"github.com/satlayer/satlayer-bvs/bvs-api/chainio/types"
-	apilogger "github.com/satlayer/satlayer-bvs/bvs-api/logger"
-	transactionprocess "github.com/satlayer/satlayer-bvs/bvs-api/metrics/indicators/transaction_process"
+	"github.com/satlayer/satlayer-bvs/bvs-cw/directory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 type ioTestSuite struct {
 	suite.Suite
-	chainIO io.ChainIO
+	chainIO   io.ChainIO
+	directory *bvs.Contract[directory.InstantiateMsg]
 }
 
 func (suite *ioTestSuite) SetupTest() {
-	chainID := "sat-bbn-testnet1"
-	rpcURI := "https://rpc.sat-bbn-testnet1.satlayer.net"
-	homeDir := "../.babylon" // Please refer to the readme to obtain
+	container := babylond.Run(context.Background())
+	suite.chainIO = container.NewChainIO("../.babylon")
+	container.FundAddressUbbn("bbn1dcpzdejnywqc4x8j5tyafv7y4pdmj7p9fmredf", 1e8)
 
-	logger := apilogger.NewMockELKLogger()
-	metricsIndicators := transactionprocess.NewPromIndicators(prometheus.NewRegistry(), "io")
-	chainIO, err := io.NewChainIO(chainID, rpcURI, homeDir, "bbn", logger, metricsIndicators, types.TxManagerParams{
-		MaxRetries:             3,
-		RetryInterval:          2 * time.Second,
-		ConfirmationTimeout:    60 * time.Second,
-		GasPriceAdjustmentRate: "1.1",
-	})
-	suite.Require().NoError(err)
-	suite.chainIO = chainIO
+	deployer := &bvs.Deployer{BabylonContainer: container}
+	delegationManager := container.GenerateAddress("throw-away")
+	suite.directory = deployer.DeployDirectory(delegationManager.String())
 }
 
 func (suite *ioTestSuite) Test_QueryContract() {
 	t := suite.T()
-	bvsDirContrAddr := "bbn1f803xuwl6l7e8jm9ld0kynvvjfhfs5trax8hmrn4wtnztglpzw0sm72xua"
 	keyName := "caller"
 	chainIO, err := suite.chainIO.SetupKeyring(keyName, "test")
 	assert.NoError(t, err)
@@ -54,7 +44,7 @@ func (suite *ioTestSuite) Test_QueryContract() {
 	}})
 	assert.NoError(t, err, "marshal query msg")
 	QueryOptions := types.QueryOptions{
-		ContractAddr: bvsDirContrAddr,
+		ContractAddr: suite.directory.Address,
 		QueryMsg:     queryMsg,
 	}
 	resp, err := chainIO.QueryContract(QueryOptions)
@@ -65,14 +55,13 @@ func (suite *ioTestSuite) Test_QueryContract() {
 
 func (suite *ioTestSuite) Test_QueryTransaction() {
 	t := suite.T()
-	bvsDirContrAddr := "bbn1f803xuwl6l7e8jm9ld0kynvvjfhfs5trax8hmrn4wtnztglpzw0sm72xua"
 	chainIO, err := suite.chainIO.SetupKeyring("caller", "test")
 	assert.NoError(t, err)
 
 	executeMsgBytes, _ := json.Marshal(&types.UpdateMetadataURIReq{UpdateMetadataURI: types.UpdateMetadataURI{MetadataURI: "example.com"}})
 	assert.NoError(t, err, "marshal execute msg")
 	executeOptions := types.ExecuteOptions{
-		ContractAddr:  bvsDirContrAddr,
+		ContractAddr:  suite.directory.Address,
 		ExecuteMsg:    executeMsgBytes,
 		Funds:         "",
 		GasAdjustment: 1.2,
@@ -102,7 +91,8 @@ func (suite *ioTestSuite) Test_QueryAccount() {
 	resp, err := suite.chainIO.QueryAccount("bbn1dcpzdejnywqc4x8j5tyafv7y4pdmj7p9fmredf")
 	assert.NoError(t, err, "execute contract")
 	assert.NotNil(t, resp, "response nil")
-	t.Logf("resp:%s", base64.StdEncoding.EncodeToString(resp.GetPubKey().Bytes()))
+	assert.Nil(t, resp.GetPubKey(), "response nil")
+	//t.Logf("resp:%s", base64.StdEncoding.EncodeToString(resp.GetPubKey().Bytes()))
 
 	// query not exist address will be failed
 	resp, err = suite.chainIO.QueryAccount("bbn1dcpzdejnywqc4x8j5tyafv7y4pdmj7p9fmredc")
