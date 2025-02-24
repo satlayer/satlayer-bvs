8b4f832694067e4e10c3ad3767ab730387e89a62
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2025-02-24 12:14:50 +0800

    feat: remove bvs_driver and state_bank, moving them to "bvs-land" away from core (#266)

--- examples/squaring/squaring-contract/src/state.rs
+++ examples/squaring/squaring-contract/src/state.rs
@@ -3,7 +3,5 @@ use cw_storage_plus::{Item, Map};
 
 pub const MAX_ID: Item<u64> = Item::new("next_id");
 pub const AGGREGATOR: Item<Addr> = Item::new("aggregator");
-pub const STATE_BANK: Item<Addr> = Item::new("state_bank");
-pub const BVS_DRIVER: Item<Addr> = Item::new("bvs_driver");
 pub const CREATED_TASKS: Map<u64, i64> = Map::new("created_tasks");
 pub const RESPONDED_TASKS: Map<u64, i64> = Map::new("responded_tasks");
