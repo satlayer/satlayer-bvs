2a573892f030bc74df88592690fd0bf907df3f1e
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-02-24 18:55:21 +0700

    feat: expose base contract structs to be used with manager

--- crates/bvs-strategy-base/src/lib.rs
+++ crates/bvs-strategy-base/src/lib.rs
@@ -6,3 +6,6 @@ pub mod error;
 pub mod query;
 
 pub use crate::error::ContractError;
+
+// this is going to serve as an example for what a satlayer compitable strategy contract should be
+// establishing a baseline.
