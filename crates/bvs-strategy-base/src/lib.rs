5b7515128f76fbd59c3f062a7db8d129208ba907
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-02-24 20:03:09 +0700

    chore: address some of the PR reviews

--- crates/bvs-strategy-base/src/lib.rs
+++ crates/bvs-strategy-base/src/lib.rs
@@ -6,6 +6,3 @@ pub mod error;
 pub mod query;
 
 pub use crate::error::ContractError;
-
-// this is going to serve as an example for what a satlayer compitable strategy contract should be
-// establishing a baseline.
