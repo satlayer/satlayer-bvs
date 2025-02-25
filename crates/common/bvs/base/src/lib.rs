ef6e24d28ffa53a402264aa670b62bc60148eb27
Fuxing <loh@fuxing.dev>
2025-02-25 12:09:09 +0800

    fully mark common for deprecated

--- crates/common/bvs/base/src/lib.rs
+++ crates/common/bvs/base/src/lib.rs
@@ -1,5 +1,10 @@
+#[deprecated(note = "please use `strategy-base` instead")]
 pub mod base;
+
+#[deprecated(note = "please use `bvs-delegation-manager` instead")]
 pub mod delegation;
+
+#[deprecated(note = "please use `bvs-strategy-manager` instead")]
 pub mod strategy;
 
 #[deprecated(note = "please use `bvs-registry` instead")]
