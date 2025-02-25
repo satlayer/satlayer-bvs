7447295c4ad6c502e82bc748c904cc474b409eb9
Fuxing <loh@fuxing.dev>
2025-02-25 12:04:14 +0800

    setup deprecated

--- crates/common/bvs/base/src/lib.rs
+++ crates/common/bvs/base/src/lib.rs
@@ -2,5 +2,7 @@ pub mod base;
 pub mod delegation;
 pub mod strategy;
 
+#[deprecated(note = "please use `bvs-registry` instead")]
 pub mod pausable;
+#[deprecated(note = "please use `bvs-registry` instead")]
 pub mod roles;
