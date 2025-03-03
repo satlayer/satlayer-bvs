555d129aeaaf0a5df491c605161946e6b3147e37
Fuxing <loh@fuxing.dev>
2025-03-04 00:00:44 +0800

    fix Execute rename

--- modules/bvs-api/tests/e2e/rewards_coordinator_test.go
+++ modules/bvs-api/tests/e2e/rewards_coordinator_test.go
@@ -332,7 +332,7 @@ func (suite *rewardsTestSuite) test_CheckClaim() {
 	earnerIndex := int64(0)
 	earnerTreeProof := bytesToUints(earnerLeaf1)
 
-	leaf := rewardscoordinator.FluffyExecuteEarnerTreeMerkleLeaf{
+	leaf := rewardscoordinator.FluffyEarnerTreeMerkleLeaf{
 		Earner:          suite.caller,
 		EarnerTokenRoot: base64.StdEncoding.EncodeToString(tokenRootHash),
 	}
@@ -385,7 +385,7 @@ func (suite *rewardsTestSuite) test_ProcessClaim() {
 	earnerIndex := int64(0)
 	earnerTreeProof := bytesToUints(earnerLeaf1)
 
-	leaf := rewardscoordinator.PurpleExecuteEarnerTreeMerkleLeaf{
+	leaf := rewardscoordinator.PurpleEarnerTreeMerkleLeaf{
 		Earner:          suite.caller,
 		EarnerTokenRoot: base64.StdEncoding.EncodeToString(tokenRootHash),
 	}
