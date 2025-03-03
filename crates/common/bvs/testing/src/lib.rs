8f5cd6af85415e15d146e60c26d693e9c19fb5b9
Fuxing <loh@fuxing.dev>
2025-03-03 17:54:59 +0800

    chore: remove bvs-testing

--- crates/common/bvs/testing/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod integration;
-
-mod bvs_delegation_manager_querier;
-mod bvs_directory_querier;
-mod bvs_mock_querier;
-mod mocks;
-
-pub use bvs_mock_querier::BvsMockQuerier;
-pub use mocks::*;
