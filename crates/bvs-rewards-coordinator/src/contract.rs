142268e13efaa3bcd77285d69c0f01c578821192
KevinK <31565174+kevzzsk@users.noreply.github.com>
2025-03-06 19:06:33 +0700

    feat: add integration test to bvs-rewards-coordinator (#392)

--- crates/bvs-rewards-coordinator/src/contract.rs
+++ crates/bvs-rewards-coordinator/src/contract.rs
@@ -317,7 +317,7 @@ pub fn process_claim(
         response = response.add_message(transfer_msg);
 
         let event = Event::new("RewardsClaimed")
-            .add_attribute("root", format!("{:?}", root.root))
+            .add_attribute("root", root.root.to_hex())
             .add_attribute("earner", earner.to_string())
             .add_attribute("claimer", claimer.to_string())
             .add_attribute("recipient", recipient.to_string())
@@ -2357,10 +2357,7 @@ mod tests {
         assert_eq!(event.ty, "RewardsClaimed");
         assert_eq!(event.attributes.len(), 6);
         assert_eq!(event.attributes[0].key, "root");
-        assert_eq!(
-            event.attributes[0].value,
-            format!("{:?}", distribution_root.root)
-        );
+        assert_eq!(event.attributes[0].value, distribution_root.root.to_hex());
         assert_eq!(event.attributes[1].key, "earner");
         assert_eq!(
             event.attributes[1].value,
