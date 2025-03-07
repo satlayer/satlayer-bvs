92259204b596804e3dd981bfca64aa0e752cfe2b
Fuxing <loh@fuxing.dev>
2025-03-07 19:15:59 +0800

    setup skeleton code

--- crates/bvs-vault-bank/src/lib.rs
+++ crates/bvs-vault-bank/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod contract;
+pub mod msg;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod error;
+mod state;
+pub use crate::error::ContractError;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod testing;
