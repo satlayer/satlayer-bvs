63bf38d658da22fdce0a4024ec772e06a60b8a51
Fuxing <loh@fuxing.dev>
2025-03-10 01:46:53 +0800

    chore(docs): document bvs-vault-router

--- crates/bvs-vault-router/src/state.rs
+++ crates/bvs-vault-router/src/state.rs
@@ -2,6 +2,7 @@ use cosmwasm_schema::cw_serde;
 use cosmwasm_std::Addr;
 use cw_storage_plus::Map;
 
+/// Mapping of vault's Addr to Vault
 pub const VAULTS: Map<&Addr, Vault> = Map::new("vaults");
 
 #[cw_serde]
