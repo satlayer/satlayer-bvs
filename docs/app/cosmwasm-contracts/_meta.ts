4d1719d49a97fc5e1c5213a041f59d3a0ef34f06
Fuxing <loh@fuxing.dev>
2025-03-13 15:23:33 +0800

    link up the docs

--- docs/app/cosmwasm-contracts/_meta.ts
+++ docs/app/cosmwasm-contracts/_meta.ts
@@ -1,5 +1,13 @@
 import type { MetaRecord } from "nextra";
 
-const meta: MetaRecord = {};
+const meta: MetaRecord = {
+  pauser: {},
+  "vault-router": {},
+  "vault-bank": {},
+  "vault-cw20": {},
+  registry: {},
+  library: {},
+  runbook: {},
+};
 
 export default meta;
