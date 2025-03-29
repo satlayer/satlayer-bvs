fbd007a273f8942fd55eb850b115c67d3f9f1937
kevin <kevin@satlayer.io>
2025-03-29 13:47:41 +0800

    move bvs-rewards contract to crates + basic impl

---
+++ crates/bvs-rewards/src/error.rs
@@ -0,0 +1,24 @@
+use cosmwasm_std::StdError;
+use cw_utils::PaymentError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum RewardsError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("Payment error: {0}")]
+    Payment(#[from] PaymentError),
+
+    #[error("Funds sent do not match the funds received")]
+    FundsMismatch {},
+
+    #[error("Merkle proof verification failed")]
+    InvalidProof {},
+
+    #[error("Insufficient balance")]
+    InsufficientBalance {},
+
+    #[error("Rewards already claimed")]
+    AlreadyClaimed {},
+}
--- crates/bvs-rewards/src/lib.rs
+++ crates/bvs-rewards/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod contract;
 pub mod msg;
+pub mod testing;
 
 mod error;
+mod merkle;
 mod state;
