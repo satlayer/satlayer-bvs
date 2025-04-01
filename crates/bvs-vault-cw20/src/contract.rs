5f907e36bdf2320f6456aa23fa4224682be24b9d
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2025-04-02 00:05:01 +0800

    chore: cw20 variant warning (#508)

--- crates/bvs-vault-cw20/src/contract.rs
+++ crates/bvs-vault-cw20/src/contract.rs
@@ -87,6 +87,14 @@ mod execute {
     ///
     /// New shares are minted, based on the exchange rate, to `msg.recipient`.  
     /// The `TOTAL_SHARE` in the vault is increased.
+    ///
+    /// ### CW20 Variant Warning
+    ///
+    /// Underlying assets that are not strictly CW20 compliant may cause unexpected behavior in token balances.
+    /// For example, any token with a fee-on-transfer mechanism is not supported.
+    ///
+    /// Therefore, we do not support non-standard CW20 tokens.
+    /// Vault deployed with such tokens will be blacklisted in the vault-router.
     pub fn deposit_for(
         deps: DepsMut,
         env: Env,
