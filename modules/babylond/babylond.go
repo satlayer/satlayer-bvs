53dd5088744f15801e96a651872948a88b626427
Fuxing <loh@fuxing.dev>
2025-04-02 18:08:55 +0800

    chore: bump babylonlabs/babylond:v1.0.0

--- modules/babylond/babylond.go
+++ modules/babylond/babylond.go
@@ -160,7 +160,7 @@ func Run(ctx context.Context) *BabylonContainer {
 
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: testcontainers.ContainerRequest{
-			Image: "babylonlabs/babylond:v1.0.0-rc.6",
+			Image: "babylonlabs/babylond:v1.0.0",
 			Entrypoint: []string{
 				"sh",
 				"-c",
