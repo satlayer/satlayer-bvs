f471494f94d1463657db3e4a9f1c6936d3b20f58
VM <dylan@satlayer.io>
2025-04-22 22:28:36 +0800

    feat: implement wasm module to save contract info

--- modules/cosmwasm-indexer/database/utils/wasm.go
+++ modules/cosmwasm-indexer/database/utils/wasm.go
@@ -1,6 +1,9 @@
 package utils
 
-import "github.com/satlayer/satlayer-bvs/cosmwasm-indexer/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/satlayer/satlayer-bvs/cosmwasm-indexer/types"
+)
 
 const (
 	maxPostgreSQLParams = 65535
@@ -36,4 +39,24 @@ func SplitWasmExecuteContracts(executeContracts []types.WasmExecuteContract, par
 	}
 
 	return slices
+}
+
+// FindEventByType returns the event with the given type
+func FindEventByType(events sdk.StringEvents, eventType string) (sdk.StringEvent, bool) {
+	for _, event := range events {
+		if event.Type == eventType {
+			return event, true
+		}
+	}
+	return sdk.StringEvent{}, false
+}
+
+// FindAttributeByKey returns the attribute with the given key
+func FindAttributeByKey(event sdk.StringEvent, key string) (sdk.Attribute, bool) {
+	for _, attribute := range event.Attributes {
+		if attribute.Key == key {
+			return attribute, true
+		}
+	}
+	return sdk.Attribute{}, false
 }
