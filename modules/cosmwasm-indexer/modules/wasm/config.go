169a52e89ce92689611b6b6124181de30813b6dc
VM <dylan@satlayer.io>
2025-04-25 22:39:17 +0800

    feat: wasm module listens to specified contracts and code id

--- modules/cosmwasm-indexer/modules/wasm/config.go
+++ modules/cosmwasm-indexer/modules/wasm/config.go
@@ -5,27 +5,36 @@ import (
 )
 
 // Config specify contract addresses that want to listen.
-// key is contract name, value is contract address.
-type Config map[string]string
+// key is contract address, value is contract label name.
+type Config struct {
+	contracts map[string]string `yaml:"contracts"`
+	codeID    []uint64          `yaml:"code_id"`
+}
 
 // NewConfig returns wasm module config instance.
-func NewConfig(contracts map[string]string) Config {
-	return contracts
+func NewConfig(contracts map[string]string, codeID []uint64) Config {
+	return Config{
+		contracts: contracts,
+		codeID:    codeID,
+	}
 }
 
 // DefaultConfig returns the default wasm module config.
 func DefaultConfig() Config {
-	contracts := map[string]string{}
-	return NewConfig(contracts)
+	contracts := map[string]string{
+		"ccc": "cw20",
+	}
+	codeID := []uint64{1, 2, 3}
+	return NewConfig(contracts, codeID)
 }
 
 // ParseConfig parses wasm config from yaml config file
-func ParseConfig(bz []byte) (Config, error) {
+func ParseConfig(bz []byte) (*Config, error) {
 	type T struct {
-		Config *Config `yaml:"wasm"`
+		Config *Config `yaml:"wasms"`
 	}
 
 	var cfg T
 	err := yaml.Unmarshal(bz, &cfg)
-	return *cfg.Config, err
+	return cfg.Config, err
 }
