b6582c523e953bbaea38b7a7b2fc6891efb14392
VM <dylan@satlayer.io>
2025-04-26 13:38:39 +0800

    fix: validate wasm module config

--- modules/cosmwasm-indexer/modules/wasm/config.go
+++ modules/cosmwasm-indexer/modules/wasm/config.go
@@ -8,24 +8,24 @@ import (
 
 // Config specify contract addresses that want to listen.
 // The key of Contracts is contract address, value is contract label name.
-// CodeID is used to fill the specified code id.
+// CodeIDs is used to fill the specified code id.
 type Config struct {
 	Contracts map[string]string `yaml:"contracts"`
-	CodeID    CodeIDSlice       `yaml:"code_id"`
+	CodeIDs   Uint64Slice       `yaml:"code_ids"`
 }
 
 // NewConfig returns wasm module config instance.
-func NewConfig(contracts map[string]string, codeID []uint64) Config {
+func NewConfig(contracts map[string]string, codeIDs []uint64) Config {
 	return Config{
 		Contracts: contracts,
-		CodeID:    codeID,
+		CodeIDs:   codeIDs,
 	}
 }
 
 // DefaultConfig returns the default wasm module config.
 func DefaultConfig() Config {
 	contracts := map[string]string{
-		"contract_address": "contract_label",
+		"contract_address": "contract_label_name",
 	}
 	codeID := []uint64{0}
 	return NewConfig(contracts, codeID)
@@ -42,16 +42,16 @@ func ParseConfig(bz []byte) (*Config, error) {
 	return cfg.Config, err
 }
 
-// CodeIDSlice is alias for uint64 slice
-type CodeIDSlice []uint64
+// Uint64Slice is used for YAML marshal/unmarshal.
+type Uint64Slice []uint64
 
-func (c CodeIDSlice) MarshalYAML() (any, error) {
+func (u Uint64Slice) MarshalYAML() (any, error) {
 	return &yaml.Node{
 		Kind: yaml.SequenceNode,
 		Tag:  "!!seq",
 		Content: func() []*yaml.Node {
-			nodes := make([]*yaml.Node, len(c))
-			for i, v := range c {
+			nodes := make([]*yaml.Node, len(u))
+			for i, v := range u {
 				nodes[i] = &yaml.Node{
 					Kind:  yaml.ScalarNode,
 					Tag:   "!!int",
