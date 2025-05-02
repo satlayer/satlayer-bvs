b82a395eeb74735bbc3bb504c29ac3e15c2208b7
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-05-02 13:14:17 +0700

    chore: update comment

--- crates/bvs-vault-cw20-tokenized/src/contract.rs
+++ crates/bvs-vault-cw20-tokenized/src/contract.rs
@@ -276,7 +276,6 @@ mod vault_execute {
 
     /// Withdraw assets from the vault by burning receipt token.
     /// The resulting staked assets are now unstaked and transferred to `msg.recipient`.  
-    /// The `TOTAL_SHARE` (aka `TOTAL_SUPPLY`) in the vault is reduced.  
     pub fn withdraw_to(
         mut deps: DepsMut,
         env: Env,
