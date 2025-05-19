4925796d37449bbdd5ebd278fcfb1834ba658501
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2025-05-19 14:36:48 +0700

    Update crates/bvs-pauser/src/state.rs
    Co-authored-by: KevinK <31565174+kevzzsk@users.noreply.github.com>
    Signed-off-by: Kaung Myat Thu <2962000kaungmyatthu@gmail.com>

--- crates/bvs-pauser/src/state.rs
+++ crates/bvs-pauser/src/state.rs
@@ -1,7 +1,7 @@
 use cosmwasm_std::Addr;
 use cw_storage_plus::{Item, Map};
 
-/// The PAUSED_PAUSED_CONTRACT_METHOD state contains the information for which method are paused on a particular contract
+/// The PAUSED_CONTRACT_METHOD state contains the information for which method are paused on a particular contract
 /// Key (composite): (contract_addr, method_msg)
 /// Value: u64, the block height at which the method was paused
 pub(crate) const PAUSED_CONTRACT_METHOD: Map<(&Addr, &String), u64> =
