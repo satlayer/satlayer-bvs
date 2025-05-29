a8c5a12a81c9fa850fe34570fc4d5cbc761ccdc2
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2025-05-29 18:08:37 +0800

    chore: use commonjs for package.js (#693)

--- packages/cli/package.js
+++ packages/cli/package.js
@@ -1,5 +1,5 @@
-import { cpSync, mkdirSync, readFileSync, writeFileSync } from "node:fs";
-import { join } from "node:path";
+const { cpSync } = require("node:fs");
+const { join } = require("node:path");
 
 const packages = ["darwin-arm64", "darwin-x64", "linux-arm64", "linux-x64", "win32-arm64", "win32-x64"];
 
