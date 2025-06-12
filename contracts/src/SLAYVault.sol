7d8500a0b056ce8e1aea78b2e9e9f55c9d2f774d
Fuxing <loh@fuxing.dev>
2025-06-12 13:54:13 +0800

    setup basic deploy

--- contracts/src/SLAYVault.sol
+++ contracts/src/SLAYVault.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.24;
 
-import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
-import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import {ERC165Upgradeable} from "openzeppelin-contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
+import {ERC20Upgradeable} from "openzeppelin-contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ERC4626Upgradeable} from "openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import {ERC20PermitUpgradeable} from
+    "openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 
-contract SLAYVault is OwnableUpgradeable {
-    uint8 public decimals = 99;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+contract SLAYVault is Initializable, ERC20Upgradeable, ERC4626Upgradeable, ERC165Upgradeable, ERC20PermitUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
+    }
+
+    function initialize(IERC20 asset_, string memory name_, string memory symbol_) public initializer {
+        __ERC4626_init(asset_);
+        __ERC20_init(name_, symbol_);
+        __ERC20Permit_init(name_);
+    }
+
+    function decimals() public view override(ERC20Upgradeable, ERC4626Upgradeable) returns (uint8) {
+        return ERC4626Upgradeable.decimals();
     }
 }
--- contracts/test/SLAYVault.t.sol
+++ contracts/test/SLAYVault.t.sol
@@ -1,19 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import "./MockERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SLAYVault} from "../src/SLAYVault.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract SLAYVaultTest is Test {
+    address public proxy;
+    MockERC20 public token;
     SLAYVault public vault;
 
     function setUp() public {
-        vault = new SLAYVault();
+        token = new MockERC20("Mock Token", "MTK", 18);
+        proxy = Upgrades.deployUUPSProxy(
+            "SLAYVault.sol:SLAYVault", abi.encodeCall(SLAYVault.initialize, (token, "SLAY TokenName", "SLAY.MTK"))
+        );
+        vault = SLAYVault(proxy);
     }
 
     function testDecimals() public {
-        uint8 expectedDecimals = 99;
-        uint8 actualDecimals = vault.decimals();
-        assertEq(actualDecimals, expectedDecimals, "Decimals should be 99");
+        assertEq(vault.decimals(), 18);
     }
 }
