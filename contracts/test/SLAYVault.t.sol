03d5dd673a085298ad9da7a065e900d4e3884f4b
Fuxing <loh@fuxing.dev>
2025-06-12 14:49:51 +0800

    use UnsafeUpgrades

--- contracts/test/SLAYVault.t.sol
+++ contracts/test/SLAYVault.t.sol
@@ -5,17 +5,19 @@ import "./MockERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SLAYVault} from "../src/SLAYVault.sol";
 import {Test, console} from "forge-std/Test.sol";
-import {Upgrades} from "@openzeppelin/foundry-upgrades/Upgrades.sol";
+import {UnsafeUpgrades} from "@openzeppelin/foundry-upgrades/Upgrades.sol";
 
 contract SLAYVaultTest is Test {
+    SLAYVault public implementation = new SLAYVault();
+
     address public proxy;
     MockERC20 public token;
     SLAYVault public vault;
 
     function setUp() public {
         token = new MockERC20("Mock Token", "MTK", 18);
-        proxy = Upgrades.deployUUPSProxy(
-            "SLAYVault.sol:SLAYVault", abi.encodeCall(SLAYVault.initialize, (token, "SLAY TokenName", "SLAY.MTK"))
+        proxy = UnsafeUpgrades.deployUUPSProxy(
+            address(implementation), abi.encodeCall(SLAYVault.initialize, (token, "SLAY TokenName", "SLAY.MTK"))
         );
         vault = SLAYVault(proxy);
     }
