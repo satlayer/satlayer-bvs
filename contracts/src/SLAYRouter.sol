8c7db4d86eea67bb39663c1f6f2eb7c3a253bc0e
Fuxing <loh@fuxing.dev>
2025-06-17 14:22:30 +0800

    add tests

--- contracts/src/SLAYRouter.sol
+++ contracts/src/SLAYRouter.sol
@@ -11,6 +11,13 @@ import {SLAYRegistry} from "./SLAYRegistry.sol";
 contract SLAYRouter is Initializable, UUPSUpgradeable, OwnableUpgradeable, PausableUpgradeable {
     SLAYRegistry public immutable registry;
 
+    mapping(address => bool) public whitelisted;
+
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
+    event Whitelisted(address indexed vault, bool whitelisted);
+
     /**
      * @dev Set the immutable SLAYRegistry proxy address for the implementation.
      * Cyclic params in constructor are possible as an EmptyImpl is used for an initial deployment,
@@ -28,4 +35,28 @@ contract SLAYRouter is Initializable, UUPSUpgradeable, OwnableUpgradeable, Pausa
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    /**
+     * @dev Pauses the contract, all SLAYVaults will also be paused.
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses the contract, all SLAYVaults will also be unpaused.
+     */
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /**
+     * Set a individual whitelist status for a vault.
+     * This allows CA owner to control which vaults can be interacted with through the router.
+     * For non-granular state/modifier, use {SLAYRouter-pause} to pause all vaults.
+     */
+    function setWhitelist(address vault_, bool whitelisted_) external onlyOwner {
+        whitelisted[vault_] = whitelisted_;
+        emit Whitelisted(vault_, whitelisted_);
+    }
 }
--- contracts/test/SLAYRouter.t.sol
+++ contracts/test/SLAYRouter.t.sol
@@ -1,12 +1,72 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
+import "../src/SLAYRouter.sol";
+import "../src/SLAYVault.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {TestSuite} from "./TestSuite.sol";
 
 contract SLAYRouterTest is Test, TestSuite {
-    function test() public view {
+    function test_defaults() public view {
         assertEq(router.owner(), owner);
         assertEq(router.paused(), false);
+    }
+
+    function test_paused() public {
+        vm.startPrank(owner);
+        router.pause();
+        vm.stopPrank();
+
+        assertTrue(router.paused());
+    }
+
+    function test_pausedOnlyOwnerError() public {
+        vm.expectRevert(abi.encodeWithSelector(OwnableUpgradeable.OwnableUnauthorizedAccount.selector, address(this)));
+        router.pause();
+    }
+
+    function test_unpausedOnlyOwnerError() public {
+        vm.startPrank(owner);
+        router.pause();
+        vm.stopPrank();
+
+        vm.expectRevert(abi.encodeWithSelector(OwnableUpgradeable.OwnableUnauthorizedAccount.selector, address(this)));
+        router.unpause();
+    }
+
+    function test_Whitelisted() public {
+        address vault = address(newVault());
+        assertFalse(router.whitelisted(vault));
+
+        vm.startPrank(owner);
+
+        vm.expectEmit();
+        emit SLAYRouter.Whitelisted(vault, true);
+        router.setWhitelist(vault, true);
+
+        assertTrue(router.whitelisted(vault));
+
+        vm.expectEmit();
+        emit SLAYRouter.Whitelisted(vault, false);
+        router.setWhitelist(vault, false);
+
+        assertFalse(router.whitelisted(vault));
+    }
+
+    /**
+     * We allow whitelisting of fake vaults, which are not created by the factory.
+     */
+    function test_WhitelistedFakeVault() public {
+        address vault = vm.randomAddress();
+        assertFalse(router.whitelisted(vault));
+
+        vm.startPrank(owner);
+
+        vm.expectEmit();
+        emit SLAYRouter.Whitelisted(vault, true);
+        router.setWhitelist(vault, true);
+
+        assertTrue(router.whitelisted(vault));
     }
 }
