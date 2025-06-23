41b6a1e44c5b5f3a47e56d9a7c9ce8622e92d980
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-06-23 15:03:02 +0700

    chore: PR reviews

--- crates/bvs-vault-factory/tests/migration_test.rs
+++ crates/bvs-vault-factory/tests/migration_test.rs
@@ -2,11 +2,164 @@ use bvs_library::testing::TestingContract;
 use bvs_pauser::testing::PauserContract;
 use bvs_registry::testing::RegistryContract;
 use bvs_vault_factory::msg::VaultType;
-use bvs_vault_factory::testing::{new_contract, old_contract, VaultFactoryContract};
+use bvs_vault_factory::testing::VaultFactoryContract;
 use bvs_vault_router::testing::VaultRouterContract;
 use cosmwasm_std::to_json_binary;
 use cosmwasm_std::{testing::mock_env, Empty};
 use cw_multi_test::{App, Contract};
+
+pub mod old_contract {
+    use cosmwasm_schema::cw_serde;
+    use cosmwasm_std::entry_point;
+    use cosmwasm_std::StdError;
+    use cosmwasm_std::{Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response};
+    use cw_multi_test::{Contract, ContractWrapper};
+    use cw_storage_plus::Item;
+    use thiserror::Error;
+
+    static FOO: Item<String> = Item::new("foo");
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn instantiate(
+        deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        _msg: bvs_vault_bank::msg::InstantiateMsg,
+    ) -> Result<Response, ContractError> {
+        cw2::set_contract_version(deps.storage, "mocked_empty_contract", "1.0.0")?;
+
+        let s = String::from("Foo");
+
+        FOO.save(deps.storage, &s)?;
+
+        Ok(Response::new())
+    }
+
+    #[derive(Error, Debug)]
+    pub enum ContractError {
+        #[error("{0}")]
+        Std(#[from] StdError),
+    }
+
+    #[cw_serde]
+    pub enum QueryMsg {}
+
+    #[cw_serde]
+    pub enum ExecuteMsg {}
+
+    #[cw_serde]
+    pub struct MigrateMsg {}
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> Result<Binary, ContractError> {
+        Err(ContractError::Std(StdError::generic_err("Not implemented")))
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn execute(
+        _deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        _msg: ExecuteMsg,
+    ) -> Result<Response, ContractError> {
+        Err(ContractError::Std(StdError::generic_err("Not implemented")))
+    }
+
+    pub fn wrapper() -> Box<dyn Contract<Empty>> {
+        Box::new(ContractWrapper::new(execute, instantiate, query))
+    }
+}
+
+pub mod new_contract {
+    use cosmwasm_schema::{cw_serde, QueryResponses};
+    use cosmwasm_std::entry_point;
+    use cosmwasm_std::StdError;
+    use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response};
+    use cw_multi_test::{Contract, ContractWrapper};
+    use cw_storage_plus::Item;
+    use thiserror::Error;
+
+    static FOO: Item<String> = Item::new("foo");
+    static BAR: Item<String> = Item::new("bar");
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn instantiate(
+        deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        _msg: bvs_vault_bank::msg::InstantiateMsg,
+    ) -> Result<Response, ContractError> {
+        cw2::set_contract_version(deps.storage, "mocked_empty_contract", "1.0.0")?;
+
+        let s = String::from("Foo");
+
+        FOO.save(deps.storage, &s)?;
+
+        Ok(Response::new())
+    }
+
+    #[derive(Error, Debug)]
+    pub enum ContractError {
+        #[error("{0}")]
+        Std(#[from] StdError),
+    }
+
+    #[cw_serde]
+    #[derive(QueryResponses)]
+    pub enum QueryMsg {
+        #[returns(ShowStatesResponse)]
+        ShowStates {},
+    }
+
+    #[cw_serde]
+    pub struct ShowStatesResponse {
+        pub states: String,
+    }
+
+    #[cw_serde]
+    pub enum ExecuteMsg {}
+
+    #[cw_serde]
+    pub struct MigrateMsg {}
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> Result<Binary, ContractError> {
+        match _msg {
+            QueryMsg::ShowStates {} => {
+                let var_foo = FOO.load(_deps.storage)?;
+                let var_bar = BAR.load(_deps.storage)?;
+                let resp = ShowStatesResponse {
+                    states: format!("Foo: {}, Bar: {}", var_foo, var_bar),
+                };
+
+                Ok(to_json_binary(&resp)?)
+            }
+        }
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn execute(
+        _deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        _msg: ExecuteMsg,
+    ) -> Result<Response, ContractError> {
+        Err(ContractError::Std(StdError::generic_err("Not implemented")))
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+        cw2::ensure_from_older_version(deps.storage, "mocked_empty_contract", "2.0.0")?;
+
+        BAR.save(deps.storage, &String::from("Bar"))?;
+
+        Ok(Response::new())
+    }
+
+    pub fn wrapper() -> Box<dyn Contract<Empty>> {
+        Box::new(ContractWrapper::new(execute, instantiate, query).with_migrate(migrate))
+    }
+}
 
 struct TestContracts {
     vault_factory: VaultFactoryContract,
