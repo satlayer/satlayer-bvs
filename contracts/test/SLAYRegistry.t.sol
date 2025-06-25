6b82019bd85f22811f17908b7c9afc33bb234ece
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-06-25 15:07:15 +0700

    fix: broken test

--- contracts/test/SLAYRegistry.t.sol
+++ contracts/test/SLAYRegistry.t.sol
@@ -474,24 +474,17 @@ contract SLAYRegistryTest is Test, TestSuite {
 
         vm.prank(service);
         registry.enableSlashing(
-            SlashParameter.object({destination: destination, maxMilliBips: 9999999, resolutionWindow: resolutionWindow})
+            SlashParameter.object({destination: destination, maxMilliBips: 10000000, resolutionWindow: resolutionWindow})
         );
 
         SlashParameter.object memory param = registry.getSlashingParameter(service);
-        assertEq(param.maxMilliBips, 9999999, "MaxBips should be 9999999");
+        assertEq(param.maxMilliBips, 10000000, "MaxBips should be 10000000");
 
         // Test maxBips at 10000001 (revert)
         vm.prank(service);
         vm.expectRevert("Maximum Milli-Bips cannot be more than 10_000_000 (100%)");
         registry.enableSlashing(
             SlashParameter.object({destination: destination, maxMilliBips: 10000001, resolutionWindow: resolutionWindow})
-        );
-
-        // Test maxBips at 0 (revert)
-        vm.prank(service);
-        vm.expectRevert("Minimum Bips cannot be less than zero");
-        registry.enableSlashing(
-            SlashParameter.object({destination: destination, maxMilliBips: 0, resolutionWindow: resolutionWindow})
         );
     }
 
@@ -585,7 +578,7 @@ contract SLAYRegistryTest is Test, TestSuite {
 
         address nonService = makeAddr("nonService");
 
-        vm.expectRevert(abi.encodeWithSelector(SLAYRegistry.ServiceNotFound.selector, nonService));
+        vm.expectRevert("RegistrationStatus not Active");
         registry.slashingOptIn(nonService);
     }
 
@@ -595,7 +588,7 @@ contract SLAYRegistryTest is Test, TestSuite {
 
         address nonOperator = makeAddr("nonOperator");
         vm.prank(nonOperator);
-        vm.expectRevert(abi.encodeWithSelector(SLAYRegistry.OperatorNotFound.selector, nonOperator));
+        vm.expectRevert("RegistrationStatus not Active");
         registry.slashingOptIn(service);
     }
 
