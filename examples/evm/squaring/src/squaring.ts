d807b2559c015788c9a161891831f9254eb80e62
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-08-05 18:50:44 +0700

    wip: offchain tests

---
+++ examples/evm/squaring/src/squaring.ts
@@ -0,0 +1,7 @@
+/**
+ * This function is an example of an expensive computation.
+ * You want to perform this off-chain to reduce on-chain costs.
+ */
+function compute(input: number): number {
+  return input * input;
+}
