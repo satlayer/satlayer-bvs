f4ac0d957bda8565076613a0c82368a762ea852d
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-08-05 18:50:44 +0700

    wip: offchain tests

---
+++ examples/evm/squaring/src/squaring.test.ts
@@ -0,0 +1,4 @@
+import { AnvilContainer, ChainName, EVMContracts, StartedAnvilContainer } from "@satlayer/testcontainers";
+let ethNodeStarted: StartedAnvilContainer;
+let opNodeStarted: StartedAnvilContainer;
+let contracts: EVMContracts;
---
+++ examples/evm/squaring/test/BVS.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TestSuiteV2} from "./TestSuite.t.sol";
+import {BVS} from "../src/BVS.sol";
+import { ISLAYRouterSlashingV2 } from "@satlayer/contracts/interface/ISLAYRouterSlashingV2.sol";
+import { ISLAYRegistryV2 } from "@satlayer/contracts/interface/ISLAYRegistryV2.sol";
+
+contract BVSTest is Test, TestSuiteV2 {
+    BVS public service;
+    address public immutable operator = makeAddr("operator");
+    address public immutable onlyOwner = makeAddr("owner");
+
+    function setUp() public override {
+        TestSuiteV2.setUp();
+        vm.prank(owner);
+        service = new BVS(router, registry);
+
+
+        vm.startPrank(operator);
+        registry.registerAsOperator("www.uri.com","A name");
+        registry.registerServiceToOperator(address(service));
+        registry.setWithdrawalDelay(10 days);
+        vm.stopPrank();
+
+        vm.prank(owner);
+        service.enableSlashing(ISLAYRegistryV2.SlashParameter({
+            destination: address(service),
+            maxMbips: 100_000,
+            resolutionWindow: 10 days
+        }));
+    }
+
+    function test_request_respond() public {
+        vm.prank(owner);
+        service.registerOperator(operator);
+
+        vm.startPrank(operator);
+        service.request(2);
+
+        _advanceBlockBy(10);
+        service.respond(2, 4);
+        vm.stopPrank();
+
+        int64 out = service.getResponse(2, operator);
+        assert(out == 4);
+    }
+
+    function test_invalid_proof_being_requested_slashing() public returns(bytes32) {
+        _advanceBlockBy(100_000_000);
+
+        vm.prank(owner);
+        service.registerOperator(operator);
+
+        vm.startPrank(operator);
+        service.request(2);
+
+        _advanceBlockBy(10);
+
+        // wrong output
+        service.respond(2, 8);
+        vm.stopPrank();
+
+        int64 out = service.getResponse(2, operator);
+
+        // assert that wrong output got into the contract states.
+        assert(out == 8);
+
+        _advanceBlockBy(10);
+
+        address anyone = makeAddr("anyone");
+        vm.prank(anyone);
+        bytes32 slashId = service.compute(2, operator);
+
+        vm.prank(anyone);
+        ISLAYRouterSlashingV2.Request memory pendingSlashRequest = router.getPendingSlashingRequest(address(service), operator);
+        ISLAYRouterSlashingV2.Request memory slashRequestById = router.getSlashingRequest(slashId);
+
+        assert(pendingSlashRequest.operator == slashRequestById.operator);
+
+        return slashId;
+    }
+}
