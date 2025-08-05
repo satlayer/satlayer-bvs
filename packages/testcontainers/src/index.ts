b45734314c2835560dc4b3de7fcd21fd67974938
KevinK <31565174+kevzzsk@users.noreply.github.com>
2025-08-05 15:14:33 +0800

    chore: export mockerc20 for abi (#925)

--- packages/testcontainers/src/index.ts
+++ packages/testcontainers/src/index.ts
@@ -1,4 +1,5 @@
 export { AnvilContainer, ChainName, saltToHex, StartedAnvilContainer, SuperTestClient } from "./anvil-container";
 export { CosmWasmContract, CosmWasmContracts } from "./cosmwasm-contracts";
 export { EVMContracts } from "./evm-contracts";
+export { default as mockerc20 } from "./MockERC20.sol/MockERC20.json";
 export { CosmWasmContainer, StartedCosmWasmContainer } from "./wasmd-container";
