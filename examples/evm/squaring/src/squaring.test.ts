b6830e93487dc588e2065879fbae2ae6eda581a3
kgmyatthu <2962000kaungmyatthu@gmail.com>
2025-08-08 02:44:48 +0700

    chore: squaring example

--- examples/evm/squaring/src/squaring.test.ts
+++ examples/evm/squaring/src/squaring.test.ts
@@ -1,24 +1,34 @@
-import { AnvilContainer, ChainName, EVMContracts, StartedAnvilContainer } from "@satlayer/testcontainers";
-import bvs from "./contracts/out/BVS.sol/BVS.json";
-import { encodeFunctionData, getContract, pad, padHex, parseEther } from "viem";
-import { beforeAll, expect, test, vi } from "vitest";
+import {
+  AnvilContainer,
+  ChainName,
+  EVMContracts,
+  StartedAnvilContainer,
+  SuperTestClient,
+} from "@satlayer/testcontainers";
+import { abi, bytecode } from "./contracts/out/BVS.sol/BVS.json";
+import { Account, encodeFunctionData, getContract, pad, padHex, parseEther } from "viem";
+import { afterAll, beforeAll, expect, test, vi } from "vitest";
+import { randomBytes } from "node:crypto";
+import { ServiceNode, SquaringNode } from "./squaring";
 
 let ethNodeStarted: StartedAnvilContainer;
-let opNodeStarted: StartedAnvilContainer;
 let contracts: EVMContracts;
 
-let ownerAddress: `0x${string}`;
-let operatorAddress: `0x${string}`;
+let owner: Account;
+let operator: Account;
+let anyone: Account;
 let bvsContract: Awaited<ReturnType<typeof ethNodeStarted.deployContract>>;
+let squaringNode: SquaringNode;
 
 async function initBVS(ethNodeStarted: StartedAnvilContainer): Promise<void> {
   bvsContract = await ethNodeStarted.deployContract({
-    abi: bvs.abi,
-    bytecode: bvs.bytecode.object as unknown as `0x${string}`,
-    salt: "bvs",
-    constructorArgs: [contracts.registry.address, contracts.router.address],
+    abi: abi,
+    salt: "squaring-contract",
+    bytecode: bytecode.object as unknown as `0x${string}`,
+    constructorArgs: [contracts.router.address, contracts.registry.address, owner.address],
   });
 
+  await ethNodeStarted.mineBlock(1);
   await bvsContract.contract.write.enableSlashing([
     {
       destination: bvsContract.contractAddress,
@@ -27,12 +37,20 @@ async function initBVS(ethNodeStarted: StartedAnvilContainer): Promise<void> {
     },
   ]);
 
+  await ethNodeStarted.mineBlock(1);
   // register operators to the BVS contract
-  await contracts.registry.write.registerAsOperator(["www.operator.com", "operator"], { account: operatorAddress });
+  await contracts.registry.write.registerAsOperator(["www.operator.com", "operator"], { account: operator.address });
 
+  await ethNodeStarted.mineBlock(1);
   await contracts.registry.write.registerServiceToOperator([bvsContract.contractAddress], {
-    account: operatorAddress,
+    account: operator.address,
   });
+
+  await ethNodeStarted.mineBlock(1);
+  await bvsContract.contract.write.registerOperator([operator.address], {
+    account: owner.address,
+  });
+  await ethNodeStarted.mineBlock(1);
 }
 
 beforeAll(async () => {
@@ -46,21 +64,56 @@ beforeAll(async () => {
   // setup local cosmos node with satlayer contracts
   contracts = await EVMContracts.bootstrap(ethNodeStarted);
 
-  const owner = ethNodeStarted.generateAccount("Owner");
-  const operator = ethNodeStarted.generateAccount("operator1");
-
-  ownerAddress = owner.address;
-  operatorAddress = operator.address;
+  owner = ethNodeStarted.getAccount() as unknown as Account;
+  operator = ethNodeStarted.generateAccount("operator1") as unknown as Account;
+  anyone = ethNodeStarted.generateAccount("anyone") as unknown as Account;
 
   // Fund all accounts with some gas
-  await ethNodeStarted.setBalance(ownerAddress, parseEther("1"));
-  await ethNodeStarted.setBalance(operatorAddress, parseEther("1"));
+  await ethNodeStarted.setBalance(owner.address, parseEther("1"));
+  await ethNodeStarted.setBalance(operator.address, parseEther("1"));
+  await ethNodeStarted.setBalance(anyone.address, parseEther("1"));
 
   await ethNodeStarted.mineBlock(1);
 
   // Initializes the BVS contract and registers it with the registry and operators.
   await initBVS(ethNodeStarted);
   await ethNodeStarted.mineBlock(1);
+  squaringNode = new SquaringNode(
+    ethNodeStarted.getClient(),
+    operator,
+    getContract({
+      address: bvsContract.contractAddress,
+      abi: abi,
+      client: ethNodeStarted.getClient(),
+    }),
+  );
+
+  setInterval(async () => {
+    await ethNodeStarted.mineBlock(10);
+  }, 2000);
+  void squaringNode.start(BigInt(5));
 }, 120_000);
 
-test("should compute off-chain and get response on-chain", async () => {}, 15_000);
+afterAll(async () => {
+  await ethNodeStarted.stop();
+  await squaringNode.stop();
+});
+
+test("should compute off-chain and get response on-chain", async () => {
+  const service = new ServiceNode(
+    getContract({
+      address: bvsContract.contractAddress,
+      abi: abi,
+      client: ethNodeStarted.getClient(),
+    }),
+  );
+
+  // Request the squaring node to compute the square of 99
+  await service.request(anyone.address, 99);
+
+  // Wait for the squaring node to compute the square and store the result on-chain
+  await vi.waitFor(async () => {
+    const response = await service.getResponse(99, operator.address);
+    expect(response).toEqual(9801n);
+  }, 20_000);
+}, 25_000);
