192a8377c8c739c9803237cfdf2477dc7e26f417
MarGA <marhaylin@gmail.com>
2025-09-03 11:12:39 +0200

    Update examples/evm/stablecoin_collateral/src/ConversionGateway.sol
    Co-authored-by: Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
    Signed-off-by: MarGA <marhaylin@gmail.com>

--- examples/evm/stablecoin_collateral/src/ConversionGateway.sol
+++ examples/evm/stablecoin_collateral/src/ConversionGateway.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.24;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-//import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
